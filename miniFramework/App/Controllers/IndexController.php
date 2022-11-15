141aafef4b742021813fac07584a10935f5faff3
Isaac Pires <isaac.alves.1005@gmail.com>
2022-11-15 20:49:34 -0300

    implementação do render

--- miniFramework/App/Controllers/IndexController.php
+++ miniFramework/App/Controllers/IndexController.php
@@ -2,13 +2,35 @@
 
 namespace App\Controllers;
 
+use stdClass;
+
 class indexController{
 
+  private $view;
+
+  public function __construct()
+  {
+    $this->view = new \stdClass();
+  }
+
   public function index(){
-    echo 'indexController Home';
+
+    $this->view->dados = ['arroz', 'frango', 'agua'];
+    $this->render('index');
+
   }
 
   public function sobreNos(){
-    echo 'teste sobreNos';
+    $this->view->dados = ['pizza', 'hamburguer', 'suco'];
+    $this->render('sobreNos');
+  }
+
+  protected function render($view){
+
+    $classAtual = get_class($this);
+    $arrayClass = explode(DIRECTORY_SEPARATOR, $classAtual);
+    $class = str_replace('Controller', '', $arrayClass[2]);
+
+    require_once "../App/Views/".$class."/".$view.".phtml";
   }
 }
