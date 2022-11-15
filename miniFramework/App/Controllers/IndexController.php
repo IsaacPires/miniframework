d2800bd5d0836ebb200cacbd12707a556d48c8c7
Isaac Pires <isaac.alves.1005@gmail.com>
2022-11-15 00:43:55 -0300

    first commit

---
+++ miniFramework/App/Controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class indexController{
+
+  public function index(){
+    echo 'indexController Home';
+  }
+
+  public function sobreNos(){
+    echo 'teste sobreNos';
+  }
+}
