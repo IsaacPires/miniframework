03ac3e6d3d236e6db4eb28053501638e4d83295c
Isaac Pires <isaac.alves.1005@gmail.com>
2022-12-11 16:20:05 -0300

    finalizacao da estrutura do framework

--- miniFramework/App/Controllers/IndexController.php
+++ miniFramework/App/Controllers/IndexController.php
@@ -3,18 +3,32 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use App\Models\Produto;
+use App\Models\Info;
+use MF\Model\Container;
 
 class indexController extends Action{
 
   public function index(){
+    
+    $produto = Container::getModel('Produto');
+    
+    $produtos = $produto->getProdutos();
 
-    $this->view->dados = ['arroz', 'frango', 'agua'];
+    $this->view->dados = $produtos;
+
     $this->render('index', 'layout1');
 
   }
 
   public function sobreNos(){
-    $this->view->dados = ['pizza', 'hamburguer', 'suco'];
+
+    $info = Container::getModel('Info');
+
+    $infos = $info->getInfo();
+
+    $this->view->dados = $infos;
+    
     $this->render('sobreNos', 'layout1');
   }
 
